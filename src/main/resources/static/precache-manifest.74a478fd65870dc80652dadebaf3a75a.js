d1e5f046271271ef05930cc925787719d42f31de
Nitin Ashok Pawar <npawar@altimetrik.com>
2019-10-02 19:31:25 +0530

    adds ui code

---
+++ src/main/resources/static/precache-manifest.74a478fd65870dc80652dadebaf3a75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfe49afb8c340486cf4796df75a6049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cefffa8a4456398040a5",
+    "url": "/static/css/main.db4d8796.chunk.css"
+  },
+  {
+    "revision": "111fcd7e9daeb418a93b",
+    "url": "/static/js/2.a747e0cc.chunk.js"
+  },
+  {
+    "revision": "cefffa8a4456398040a5",
+    "url": "/static/js/main.ba9bfff4.chunk.js"
+  },
+  {
+    "revision": "df61e2787a834f6bb496",
+    "url": "/static/js/runtime~main.8e6aa965.js"
+  }
+]);
